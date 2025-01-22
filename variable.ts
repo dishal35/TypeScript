e73c7d93aa23032ca9518894f3cb5ad59a562868
dishal35 <ldisha1103@gmail.com>
2025-01-22 13:30:21 +0530

    functions in typescript

---
+++ variable.ts
@@ -0,0 +1,14 @@
+let greetings:string = "Hello Disha"
+
+//greetings=6 type safety
+
+console.log(greetings)
+
+
+let userId:number=90//redundant 
+
+let userId2=90//type inference
+
+console.log(userId2);
+
+export {}
